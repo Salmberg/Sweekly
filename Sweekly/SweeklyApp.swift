a225b2923300e11d456c7fa6da9709edb8ab4c02
Salmberg <60116309+Salmberg@users.noreply.github.com>
2024-02-07 18:08:15 +0100

    Initial Commit

---
+++ Sweekly/SweeklyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SweeklyApp.swift
+//  Sweekly
+//
+//  Created by David Salmberg on 2024-02-07.
+//
+
+import SwiftUI
+
+@main
+struct SweeklyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
