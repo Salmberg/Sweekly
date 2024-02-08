dfa3a0d0cab1be1bb4f405d7c30b896f4bff3a28
Salmberg <60116309+Salmberg@users.noreply.github.com>
2024-02-08 20:54:05 +0100

    Added button to add task and a modal

---
+++ Sweekly/View/AddTaskView.swift
@@ -0,0 +1,18 @@
+//
+//  AddTaskView.swift
+//  Sweekly
+//
+//  Created by David Salmberg on 2024-02-08.
+//
+
+import SwiftUI
+
+struct AddTaskView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddTaskView()
+}
---
+++ Sweekly/ViewModel/AddTaskViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  AddTaskViewModel.swift
+//  Sweekly
+//
+//  Created by David Salmberg on 2024-02-08.
+//
+
+import Foundation
